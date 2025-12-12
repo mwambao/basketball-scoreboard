e132f59180fdc0ddb31ae09e6079c08c8a682285
Duncan <duncan.owino@gmail.com>
2025-12-12 13:45:32 +0000

    js home team score

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 let countHome = 0;
+let selectScoreHome = document.getElementById("home-score");
+selectScoreHome.textContent = countHome;
 
-function plus1Home() {
-
+function plus1Home(){
+    selectScoreHome.textContent = Number( selectScoreHome.textContent ) + 1;
 }
+
+
+function plus2Home(){
+    selectScoreHome.textContent = Number(selectScoreHome.textContent) + 2;
+}
+
+function plus3Home(){
+    selectScoreHome.textContent = Number(selectScoreHome.textContent) + 3;
+}
+
