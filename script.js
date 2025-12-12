ddc3e0b1123ea6b2a2eac052acc4907f0f00fbae
Duncan <duncan.owino@gmail.com>
2025-12-12 15:52:24 +0000

    js guest team score

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+//Home 
 let countHome = 0;
 let selectScoreHome = document.getElementById("home-score");
 selectScoreHome.textContent = countHome;
@@ -16,3 +17,21 @@ function plus3Home(){
     selectScoreHome.textContent = Number(selectScoreHome.textContent) + 3;
 }
 
+
+// Guest
+let countGuest = 0;
+let selectScoreGuest = document.getElementById("guest-score");
+selectScoreGuest.textContent = countGuest;
+
+function plus1Guest(){
+    selectScoreGuest.textContent = Number( selectScoreGuest.textContent ) + 1;
+}
+
+
+function plus2Guest(){
+    selectScoreGuest.textContent = Number( selectScoreGuest.textContent ) + 2
+}
+
+function plus3Guest(){
+    selectScoreGuest.textContent = Number( selectScoreGuest.textContent ) + 3;
+}
